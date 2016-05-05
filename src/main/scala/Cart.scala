2b7edf161e55c94ae5590a5b6f607aadbcaa6672
Diego Zambelli Sessona <diego.sessona@gmail.com>
2016-05-05 15:56:57 +0100

    solution first part with tdd and tailrec variation. groupby?

--- src/main/scala/Cart.scala
+++ src/main/scala/Cart.scala
@@ -1,19 +1,39 @@
+import scala.annotation.tailrec
+
 /**
   * Created by diego on 05/05/16.
   */
 
 trait Product {
-
+  def getPrice: Long
 }
 
-case object Apple extends Product
-case object Orange extends Product
+case object Apple extends Product {
+  override def getPrice = 60
+}
+
+case object Orange extends Product {
+  override def getPrice = 25
+}
+
+
 
 object Cart extends App {
 
-  def calculatePrice(items: List[Product]): Option[Long] = items match {
+  @tailrec
+  private def recGetTotal(items: Seq[Product], acc: Long): Option[Long] = items match {
+    case Nil => Some(acc)
+    case x :: xs => recGetTotal(xs, acc + x.getPrice)
+  }
+
+  private def getTotal(items: Seq[Product]): Option[Long] = {
+    recGetTotal(items, 0)
+    //Some(items.map(_.getPrice).sum)
+  }
+
+  def calculatePrice(items: Seq[Product]): Option[Long] = items match {
     case Nil => None
-    case x :: xs => Some(2)
+    case _ => getTotal(items)
   }
 
 }
--- src/test/scala/CartSpec.scala
+++ src/test/scala/CartSpec.scala
@@ -5,7 +5,21 @@ import org.scalatest._
 
 class CartSpec extends FlatSpec with Matchers {
 
+  val referenceCart = Seq(Apple, Apple, Orange, Apple)
+
   "The checkout" should "return a None if it is emtpy" in {
     Cart.calculatePrice(Nil) should be (None)
+  }
+
+  it should "return the price of 60 p for a cart with one apple" in {
+    Cart.calculatePrice(Seq(Apple)) should be (Some(60))
+  }
+
+  it should "return the price of 25 p for a cart with one ornge" in {
+    Cart.calculatePrice(Seq(Orange)) should be (Some(25))
+  }
+
+  it should "return the correct price for the reference cart" in {
+    Cart.calculatePrice(referenceCart) should be (Some(205))
   }
 }
